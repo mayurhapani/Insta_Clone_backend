42ee6aa341f3981294195a5fca3ec2e301f7b0d1
mayurhapani <hapanimayur@gmail.com>
2024-07-26 02:08:07 +0530

    remove temp route

--- routers/user.router.js
+++ routers/user.router.js
@@ -12,10 +12,6 @@ const {
   removeProfilePic,
 } = require("../controllers/user.controller");
 
-uRouter.get("/", (req, res) => {
-  res.send("hiii");
-});
-
 uRouter.post("/signup", signup);
 uRouter.post("/signin", login);
 
