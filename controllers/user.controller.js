1db516497b15b2d731cacc5c8a6c3b5e2803bee9
mayurhapani <hapanimayur@gmail.com>
2024-07-26 14:48:01 +0530

    logout small changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -77,7 +77,7 @@ const logout = async (req, res) => {
     });
     res.status(200).json({ message: "Logged out successfully!!!!" });
   } catch (error) {
-    return res.status(500).json({ message: "hiiiiii" });
+    return res.status(500).json({ message: error.message });
   }
 };
 
